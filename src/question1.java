001a1abbedc543234e4eeeccd579ff0efafe53aa
jivtesh-singh <sjivtesh27@gmail.com>
2022-10-18 23:27:04 +0530

    qq

--- src/question1.java
+++ src/question1.java
@@ -3,7 +3,7 @@ public class question1
     public static void main(String[] args)
     {
         String str1 = new String("Jivtesh");
-        String str2 = new String("Jivtesh");
+        String str2 = new String("Singh");
         for (int i = 0; i < str1.length() && i < str2.length(); i++)
         {
             if ((int) str1.charAt(i) == (int) str2.charAt(i))
